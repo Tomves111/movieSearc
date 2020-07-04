df9f7cdcb30de31679905138dbc3934a47b9d29f
tomves111 <tomves111@gmail.com>
2020-07-04 15:48:07 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+import './test';
+import SearchMovies from "./searchMovie";
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <h1 className="title">Movie Search</h1>
+        <SearchMovies/>
+
+    </div>
+  );
+}
+
+export default App;
